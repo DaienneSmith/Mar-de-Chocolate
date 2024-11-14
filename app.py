0cd21de174fd1f42e587e7121e17764171975a48
Daienne Smith <daienne.pma@gmail.com>
2024-11-14 09:50:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'mardechocolate'gdfhdgf dg
+app.config['MYSQL_DB'] = 'mardechocolate'
 
 # Setup da conexão com MySQL
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
